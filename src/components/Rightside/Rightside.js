10c88a5f08b4ea1ebf133491f44bd3321c38f921
Kirill <k3499@yandex.ru>
2022-11-25 19:55:11 +0300

    320 adaptive

--- src/components/Rightside/Rightside.js
+++ src/components/Rightside/Rightside.js
@@ -7,19 +7,19 @@ function Rightside() {
       <span className="rightside__title-important-free">бесплатной </span><span className="rightside__title-important-talk">консультацией</span> мы дарим:</p>
       <div className="rightside__features-box">
         <div className="rightside__features-box-item">
-          <strong className="rightside__features-box-title">Виджеты</strong>
+          <strong className="rightside__features-box-title rightside__feature-wid"></strong>
           <p className="rightside__features-box-subtitle">30 готовых решений</p>
         </div>
         <div className="rightside__features-box-item">
-          <strong className="rightside__features-box-title">Dashboard</strong>
+          <strong className="rightside__features-box-title rightside__feature-dash"></strong>
           <p className="rightside__features-box-subtitle">с показателями вашего бизнеса</p>
         </div>
         <div className="rightside__features-box-item">
-          <strong className="rightside__features-box-title">Skype Аудит</strong>
+          <strong className="rightside__features-box-title rightside__feature-skype"></strong>
           <p className="rightside__features-box-subtitle">отдела продаж и CRM системы</p>
         </div>
         <div className="rightside__features-box-item">
-          <strong className="rightside__features-box-title">35 дней</strong>
+          <strong className="rightside__features-box-title rightside__feature-free"></strong>
           <p className="rightside__features-box-subtitle">использования CRM</p>
         </div>
       </div>
